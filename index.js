3ab2c105797f1488b2c075be22f5f88baa7b16f3
mark <mark.henein9616@gmail.com>
2023-08-05 20:07:42 -0400

    Processed the xur command, edited comments

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, EmbedBuilder, GatewayIntentBits, Events } = require('discord.js');
+const { Client, EmbedBuilder, GatewayIntentBits, Events, hyperlink } = require('discord.js');
 
 const getItemInformation = require('./item-info.js')
 const { getXurInventory, getBansheeInventory } = require('./vendor-inv.js')
@@ -31,6 +31,8 @@ client.on(Events.InteractionCreate, async interaction => {
         infoCommand(interaction);
     } else if (interaction.commandName === 'gunsmith') {
         await gunsmithCommand(interaction);
+    } else if (interaction.commandName === 'xur') {
+        await xurCommand(interaction);
     }
 })
 
@@ -44,6 +46,7 @@ function infoCommand(interaction) {
         if (typeof result === 'string' || result instanceof String) {
             interaction.reply(result)
         } else {
+            // Set the colour of the embed border
             let colour = undefined
             if (result.itemRarity === 'Exotic') {
                 colour = 'Yellow';
@@ -57,6 +60,7 @@ function infoCommand(interaction) {
                 colour = 'Grey';
             }
 
+            // Building embed object
             let embed = undefined
             if (result.itemCategory === 'Weapon') {
                 embed = new EmbedBuilder()
@@ -87,7 +91,7 @@ function infoCommand(interaction) {
                         { name: 'Flavor text:', value: result.itemDescription },
                     )
             }
-            // reply to message with embed
+            // Reply to message with embed
             interaction.reply({ embeds: [embed] })
         }
     })
@@ -96,12 +100,12 @@ function infoCommand(interaction) {
 async function gunsmithCommand(interaction) {
     await interaction.reply('Processing request...');
 
-    inventory = await getBansheeInventory();
-    console.log(inventory);
+    const inventory = await getBansheeInventory();
 
     let weekly_weapons = [];
     let featured_weapons = [];
 
+    // Convert refresh date to Date object and classify as weekly or featured weapon
     for (let item in inventory) {
         let date = new Date(inventory[item]);
 
@@ -112,22 +116,48 @@ async function gunsmithCommand(interaction) {
         }
     }
 
-    console.log(weekly_weapons);
-    console.log(featured_weapons);
-
+    // Build embed object
     embed = new EmbedBuilder()
         .setTitle('Banshee-44: The Gunsmith')
         .setURL('https://www.light.gg/db/vendors/672118013/banshee-44/')
         .setThumbnail('https://www.bungie.net/common/destiny2_content/icons/d40e1942e625152534d12c23b6d1cdb3.png')
         .setDescription('Banshee-44 has lived many lives. As master weaponsmith for the Tower, he supplies Guardians with only the best.')
-        .setFooter({ text: 'Banshee-44 inventory', iconURL: 'https://www.pngkey.com/png/full/440-4408148_destiny-clipart-destiny-game-destiny.png' })
+        .setFooter({ text: "Banshee-44's inventory", iconURL: 'https://www.pngkey.com/png/full/440-4408148_destiny-clipart-destiny-game-destiny.png' })
         .setTimestamp()
 
+    // Add separate fields for weekly/featured weapons
     if (weekly_weapons.length > 0) {
         embed.addFields({ name: 'Weekly Weapons:', value: weekly_weapons.join(', ') });
     } if (featured_weapons.length > 0) {
         embed.addFields({ name: 'Featured Weapons:', value: featured_weapons.join(', ') });
     }
 
+    // Edit message by adding embed
+    await interaction.editReply({ embeds: [embed] });
+}
+
+async function xurCommand(interaction) {
+    await interaction.reply('Processing request...');
+
+    const inventory = await getXurInventory();
+
+    // Build embed object
+    embed = new EmbedBuilder()
+        .setTitle('Xûr: Agent of the Nine')
+        .setURL('https://www.light.gg/db/vendors/2190858386/x%C3%BBr/')
+        .setThumbnail('https://www.bungie.net/img/destiny_content/vendor/icons/xur_large_icon.png')
+        .setDescription("A peddler of strange curios, Xûr's motives are not his own. He bows to his distant masters, the Nine.")
+        .setFooter({ text: "Xûr's inventory", iconURL: 'https://www.pngkey.com/png/full/440-4408148_destiny-clipart-destiny-game-destiny.png' })
+        .setTimestamp()
+
+    // Add fields for Xur's sales
+    for (let item of inventory.armour) {
+        embed.addFields({ name: `${item.class} Armor`, value: `[${item.name}](https://www.light.gg/db/items/${item.hash})`, inline: true });
+    }
+    for (let item of inventory.weapons) {
+        embed.addFields({ name: 'Weapon', value: `[${item.name}](https://www.light.gg/db/items/${item.hash})`, inline: true });
+    }
+
+    // Edit message by adding embed
     await interaction.editReply({ embeds: [embed] });
 }
